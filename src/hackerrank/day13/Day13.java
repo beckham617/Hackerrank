b458bfb59ecdce499fcef804e6324abbecfba08c
Leon Lee <beckham_liang@msn.com>
2018-11-29 11:41:02 -0800

    Day 13 code

---
+++ src/hackerrank/day13/Day13.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank.day13;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author beckh
+ */
+public class Day13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
